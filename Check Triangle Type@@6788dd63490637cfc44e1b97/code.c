7c8e898ea9bbe0ff82d31a0177927271cb80dcff
Sarakshi-coder <sarakshikaushik.10e.p4034@gmail.com>
2025-02-14 23:26:06 +0530

    Solution to Check Triangle Type

--- Check Triangle Type@@6788dd63490637cfc44e1b97/code.c
+++ Check Triangle Type@@6788dd63490637cfc44e1b97/code.c
@@ -2,7 +2,6 @@
 
 int main() {
     int a, b, c;
-    printf("Enter three sides of the triangle: ");
     scanf("%d %d %d", &a, &b, &c);
     if (a + b > c && a + c > b && b + c > a) {
         if (a == b && b == c) {
