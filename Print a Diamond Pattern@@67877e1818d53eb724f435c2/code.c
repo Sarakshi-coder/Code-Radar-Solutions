8d165a5cd97a61d34515184c417c54a48744fd31
Sarakshi <sarakshikaushik.10e.p4034@gmail.com>
2025-03-31 19:48:20 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
-
 int main() {
-    int n;
-    scanf("%d", &n);
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n - i; j++) {
+    int N, i, j;
+    scanf("%d", &N);
+    for (i = -N + 1; i < N; i++) {
+        for (j = 0; j < i; j++) 
             printf(" ");
-        }
-        for (int j = 1; j <= 2 * i - 1; j++) {
+        for (j = 0; j < 2 * (N - i) - 1; j++) 
             printf("*");
-        }
-        printf("\n");
+            printf("\n");
     }
-    for (int i = n - 1; i >= 1; i--) {
-        for (int j = 1; j <= n - i; j++) {
-            printf(" ");
-        }
-        for (int j = 1; j <= 2 * i - 1; j++) {
-            printf("*");
-        }
-        printf("\n");
-    }
-
     return 0;
 }
+
+//int main() {
+    //int n;
+    //scanf("%d", &n);
+    //for (int i = 1; i <= n; i++) {
+      //  for (int j = 1; j <= n - i; j++) {
+        //    printf(" ");
+        //}
+        //for (int j = 1; j <= 2 * i - 1; j++) {
+          //  printf("*");
+        //}
+       // printf("\n");
+    //}
+    //for (int i = n - 1; i >= 1; i--) {
+      //  for (int j = 1; j <= n - i; j++) {
+        //    printf(" ");
+        //}
+        //for (int j = 1; j <= 2 * i - 1; j++) {
+          //  printf("*");
+       // }
+       // printf("\n");
+    //}
+
+    //return 0;
+//}
